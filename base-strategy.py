e7f4fd9496860698bf39c8dbfedec85d4cdf6924
Tetiana Rabiichuk <32356440+sivomke@users.noreply.github.com>
2022-02-03 11:51:58 +0300

    Update base-strategy.py
    - added printing statements to `before_training_epoch` function to track the issue

--- base-strategy.py
+++ base-strategy.py
@@ -12,10 +12,9 @@ parser.add_argument('--path_to_tensors', type=str, default='data/magnifications/
                     help="path to dataset that contains train/test datasets saved as tensors")
 parser.add_argument('--wandb_project_name', type=str, default='continual-segmentation')
 parser.add_argument('--wandb_run_name', type=str, default='base-strategy-early-stopping')
-parser.add_argument('--wandb_checkpoint_path', type=str,
-                    default='checkpoints/magnifications/longer-training/base-strategy/wandb')
 parser.add_argument("--results_path", type=str, default='checkpoints/magnifications/longer-training/base-strategy',
                     help="path to the folder where we save checkpoints")
+parser.add_argument('--early_stopping', action='store_true')
 args = parser.parse_args()
 print('arguments parsed: ')
 for arg in vars(args):
@@ -29,30 +28,133 @@ if args.gpu is not None:
     print(f'reserved gpu {os.environ.get("CUDA_VISIBLE_DEVICES")}')
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-from avalanche.benchmarks.generators import tensors_benchmark, dataset_benchmark, benchmark_with_validation_stream
+from avalanche.benchmarks.generators import dataset_benchmark, benchmark_with_validation_stream
 from avalanche.training.plugins import EvaluationPlugin, EarlyStoppingPlugin
 from avalanche.logging import WandBLogger
-from avalanche.evaluation.metrics import forgetting_metrics, accuracy_metrics, \
-    loss_metrics, timing_metrics, cpu_usage_metrics, confusion_matrix_metrics, disk_usage_metrics
-from avalanche.benchmarks.utils import AvalancheTensorDataset
+from avalanche.evaluation.metrics import loss_metrics
 from avalanche.logging import InteractiveLogger
 from avalanche.benchmarks.utils import AvalancheTensorDataset
 from model import UNET
-from avalanche.training.strategies import Naive
 from torch.nn import BCELoss
-from torch.optim import SGD, Adam
-from torch.utils.data import TensorDataset
+from torch.optim import  Adam
+from avalanche.benchmarks.utils.avalanche_dataset import AvalancheDatasetType
 import time
-import cv2
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-from PIL import Image
-from uniseg.utils import imgutils
-from torchvision.transforms import Compose, ToTensor, CenterCrop
-from tqdm import tqdm
+from torchvision.transforms import  CenterCrop
 from avalanche.training.strategies import BaseStrategy
-from avalanche.training.plugins import ReplayPlugin, EWCPlugin
+
+
+
+import operator
+import warnings
+from copy import deepcopy
+
+from avalanche.training.plugins import StrategyPlugin
+
+
+# source: https://github.com/ContinualAI/avalanche/blob/e91183e6fee8ccfdcde79f674e0c427f72bb9e8c/avalanche/training/plugins/early_stopping.py
+# I have added printing statements to `before_training_epoch` function to track the issue
+class EarlyStoppingPlugin(StrategyPlugin):
+    """Early stopping and model checkpoint plugin.
+    The plugin checks a metric and stops the training loop when the accuracy
+    on the metric stopped progressing for `patience` epochs.
+    After training, the best model's checkpoint is loaded.
+    .. warning::
+        The plugin checks the metric value, which is updated by the strategy
+        during the evaluation. This means that you must ensure that the
+        evaluation is called frequently enough during the training loop.
+        For example, if you set `patience=1`, you must also set `eval_every=1`
+        in the `BaseStrategy`, otherwise the metric won't be updated after
+        every epoch/iteration. Similarly, `peval_mode` must have the same
+        value.
+    """
+
+    def __init__(
+        self,
+        patience: int,
+        val_stream_name: str,
+        metric_name: str = "Top1_Acc_Stream",
+        mode: str = "max",
+        peval_mode: str = "epoch",
+    ):
+        """Init.
+        :param patience: Number of epochs to wait before stopping the training.
+        :param val_stream_name: Name of the validation stream to search in the
+        metrics. The corresponding stream will be used to keep track of the
+        evolution of the performance of a model.
+        :param metric_name: The name of the metric to watch as it will be
+        reported in the evaluator.
+        :param mode: Must be "max" or "min". max (resp. min) means that the
+        given metric should me maximized (resp. minimized).
+        :param peval_mode: one of {'epoch', 'iteration'}. Decides whether the
+            early stopping should happen after `patience`
+            epochs or iterations (Default='epoch').
+        """
+        super().__init__()
+        self.val_stream_name = val_stream_name
+        self.patience = patience
+
+        assert peval_mode in {"epoch", "iteration"}
+        self.peval_mode = peval_mode
+
+        self.metric_name = metric_name
+        self.metric_key = (
+            f"{self.metric_name}/eval_phase/" f"{self.val_stream_name}"
+        )
+        print(self.metric_key)
+        if mode not in ("max", "min"):
+            raise ValueError(f'Mode must be "max" or "min", got {mode}.')
+        self.operator = operator.gt if mode == "max" else operator.lt
+
+        self.best_state = None  # Contains the best parameters
+        self.best_val = None
+        self.best_step = None
+
+    def before_training(self, strategy, **kwargs):
+        self.best_state = None
+        self.best_val = None
+        self.best_step = None
+
+    def before_training_iteration(self, strategy, **kwargs):
+        if self.peval_mode == "iteration":
+            self._update_best(strategy)
+            curr_step = self._get_strategy_counter(strategy)
+            if curr_step - self.best_step >= self.patience:
+                strategy.model.load_state_dict(self.best_state)
+                strategy.stop_training()
+
+    def before_training_epoch(self, strategy, **kwargs):
+        if self.peval_mode == "epoch":
+            self._update_best(strategy)
+            curr_step = self._get_strategy_counter(strategy)
+            print(f'Metric name: {self.metric_name}')
+            print(f'Metric key: {self.metric_key}')
+            print(f'Current step: {curr_step}, current val metric: {strategy.evaluator.get_last_metrics().get(self.metric_key)}')
+            print(f'Best step: {self.best_step}, best metric: {self.best_val}')
+            print(f'Patience: {self.patience}')
+            if curr_step - self.best_step >= self.patience:
+                strategy.model.load_state_dict(self.best_state)
+                strategy.stop_training()
+
+    def _update_best(self, strategy):
+        res = strategy.evaluator.get_last_metrics()
+        val_acc = res.get(self.metric_key)
+        if self.best_val is None:
+            warnings.warn(
+                f"Metric {self.metric_name} used by the EarlyStopping plugin "
+                f"is not computed yet. EarlyStopping will not be triggered."
+            )
+        if self.best_val is None or self.operator(val_acc, self.best_val):
+            self.best_state = deepcopy(strategy.model.state_dict())
+            self.best_val = val_acc
+            self.best_step = self._get_strategy_counter(strategy)
+
+    def _get_strategy_counter(self, strategy):
+        if self.peval_mode == "epoch":
+            return strategy.clock.train_exp_epochs
+        elif self.peval_mode == "iteration":
+            return strategy.clock.train_exp_iterations
+        else:
+            raise ValueError("Invalid `peval_mode`:", self.peval_mode)
 
 
 # loading data and creating the benchmark
@@ -97,13 +199,14 @@ exp_3_test_dataset = AvalancheTensorDataset(experience_3_test_x, experience_3_te
 
 
 # generates stream of data
-scenario_from_datasets = dataset_benchmark(
+benchmark = dataset_benchmark(
     [exp_1_train_dataset, exp_2_train_dataset, exp_3_train_dataset],
     [exp_1_test_dataset, exp_2_test_dataset, exp_3_test_dataset],
+    dataset_type=AvalancheDatasetType.SEGMENTATION
 )
 
-# benchmark, where the validation stream, created from training stream, has been added
-benchmark = benchmark_with_validation_stream(scenario_from_datasets,  input_stream = 'train',
+# # benchmark, where the validation stream, created from training stream, has been added
+benchmark = benchmark_with_validation_stream(benchmark,  input_stream = 'train',
                                      output_stream='valid', validation_size=0.25)
 
 
@@ -113,7 +216,6 @@ loggers = []
 loggers.append(InteractiveLogger())
 loggers.append(WandBLogger(project_name=args.wandb_project_name,
                            run_name=args.wandb_run_name,
-                           path=args.wandb_checkpoint_path
                           ))
 
 # evaluation logging
@@ -124,6 +226,7 @@ eval_plugin = EvaluationPlugin(
 )
 
 model = UNET(in_channels=1, out_channels=1).to(DEVICE)
+
 optimizer = Adam(model.parameters(), lr=args.lr)
 criterion = BCELoss()
 train_batch_size = args.training_batch_size
@@ -131,8 +234,11 @@ epochs = args.epochs
 eval_batch_size = args.eval_batch_size
 
 # plugins
-early_stopping = EarlyStoppingPlugin(patience=10, metric_name = 'Loss_Exp', val_stream_name='valid', mode='min',
-                                     peval_mode='epoch')
+plugins = []
+if args.early_stopping:
+    early_stopping = EarlyStoppingPlugin(patience=2, metric_name = 'Loss_Exp', val_stream_name='valid_stream', mode='min',
+                                         peval_mode='epoch')
+    plugins.append(early_stopping)
 
 # defines how the training is performed
 strategy = BaseStrategy(
@@ -143,9 +249,9 @@ strategy = BaseStrategy(
     epochs,
     eval_batch_size,
     DEVICE,
-    plugins=[early_stopping],
+    plugins=plugins,
     evaluator=eval_plugin,
-    eval_every=10,
+    eval_every=1,
     peval_mode='epoch'
 )
 
@@ -153,9 +259,9 @@ strategy = BaseStrategy(
 # training
 print('Starting experiment...')
 results = []
-for experience in benchmark.train_stream:
+for i, experience in enumerate(benchmark.train_stream):
     print("Start of experience: ", experience.current_experience)
-    res = strategy.train(experience)
+    res = strategy.train(experience, eval_streams = [benchmark.valid_stream[i]])
     print('Training completed')
 
     # save model
